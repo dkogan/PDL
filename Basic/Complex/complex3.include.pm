6dfd4b0e0e26eed8b9b0e3c189f3d26dc4fdfda4
Dima Kogan <dima@secretsauce.net>
2013-05-06 16:39:23 -0700

    removed no-longer-needed .pm code from Complex3

--- Basic/Complex/complex3.include.pm
+++ Basic/Complex/complex3.include.pm
@@ -1,147 +1,5 @@
-use Carp;
-sub cplx($) {
-   return $_[0] if UNIVERSAL::isa($_[0],'PDL::Complex'); # NOOP if just piddle
-   croak "first dimsize must be 2" unless $_[0]->dims > 0 && $_[0]->dim(0) == 2;
-   bless $_[0]->slice('');
-}
-
-sub complex($) {
-   return $_[0] if UNIVERSAL::isa($_[0],'PDL::Complex'); # NOOP if just piddle
-   croak "first dimsize must be 2" unless $_[0]->dims > 0 && $_[0]->dim(0) == 2;
-   bless $_[0];
-}
-
-*PDL::cplx = \&cplx;
-*PDL::complex = \&complex;
-
-sub real($) {
-   return $_[0] unless UNIVERSAL::isa($_[0],'PDL::Complex'); # NOOP unless complex
-   bless $_[0]->slice(''), 'PDL';
-}
-
-sub Ctan($) { Csin($_[0]) / Ccos($_[0]) }
-
-
-sub Catan($) {
-   my $z = shift;
-   Cmul Clog(Cdiv (PDL::Complex::i+$z, PDL::Complex::i-$z)), pdl(0, 0.5);
-}
-
-sub re($) { bless $_[0]->slice("(0)"), 'PDL'; }
-sub im($) { bless $_[0]->slice("(1)"), 'PDL'; }
-
-*PDL::Complex::re = \&re;
-*PDL::Complex::im = \&im;
-
-
-# overload must be here, so that all the functions can be seen
-
-# undocumented compatibility functions
-sub Catan2($$) { Catan Cdiv $_[1], $_[0] }
-sub atan2($$)  { Catan Cdiv $_[1], $_[0] }
-
-sub _gen_biop {
-   local $_ = shift;
-   my $sub;
-   if (/(\S+)\+(\w+)/) {
-      $sub = eval 'sub { '.$2.' $_[0], ref $_[1] eq __PACKAGE__ ? $_[1] : r2C $_[1] }';
-   } elsif (/(\S+)\-(\w+)/) {
-      $sub = eval 'sub { my $b = ref $_[1] eq __PACKAGE__ ? $_[1] : r2C $_[1];
-                       $_[2] ? '.$2.' $b, $_[0] : '.$2.' $_[0], $b }';
-   } else {
-      die;
-   }
-   if($1 eq "atan2" || $1 eq "<=>") { return ($1, $sub) }
-   ($1, $sub, "$1=", $sub);
-}
-
-sub _gen_unop {
-   my ($op, $func) = ($_[0] =~ /(.+)@(\w+)/);
-   *$op = \&$func if $op =~ /\w+/; # create an alias
-   ($op, eval 'sub { '.$func.' $_[0] }');
-}
-
-sub _gen_cpop {
-   ($_[0], eval 'sub { my $b = ref $_[1] eq __PACKAGE__ ? $_[1] : r2C $_[1];
-                 ($_[2] ? $b <=> $_[0] : $_[0] <=> $b) '.$_[0].' 0 }');
-}
-
-sub initialize {
-   # Bless a null PDL into the supplied 1st arg package
-   #   If 1st arg is a ref, get the package from it
-   bless PDL->null, ref($_[0]) ? ref($_[0]) : $_[0];
-}
-
-use overload
-   (map _gen_biop($_), qw(++Cadd --Csub *+Cmul /-Cdiv **-Cpow atan2-Catan2 <=>-Ccmp)),
-   (map _gen_unop($_), qw(sin@Csin cos@Ccos exp@Cexp abs@Cabs log@Clog sqrt@Csqrt abs@Cabs)),
-   (map _gen_cpop($_), qw(< <= == != >= >)),
-   '++' => sub { $_[0] += 1 },
-   '--' => sub { $_[0] -= 1 },
-   '""' => \&PDL::Complex::string
-;
-
-# overwrite PDL's overloading to honour subclass methods in + - * /
-{ package PDL;
-        my $warningFlag;
-        # This strange usage of BEGINs is to ensure the
-        # warning messages get disabled and enabled in the
-        # proper order. Without the BEGIN's the 'use overload'
-        #  would be called first.
-        BEGIN {$warningFlag = $^W; # Temporarily disable warnings caused by
-               $^W = 0;            # redefining PDL's subs
-              }
-
-
-sub cp(;@) {
-	my $foo;
-	if (ref $_[1]
-		&& (ref $_[1] ne 'PDL')
-		&& defined ($foo = overload::Method($_[1],'+')))
-		{ &$foo($_[1], $_[0], !$_[2])}
-	else { PDL::plus (@_)}
-}
-
-sub cm(;@) {
-	my $foo;
-	if (ref $_[1]
-		&& (ref $_[1] ne 'PDL')
-		&& defined ($foo = overload::Method($_[1],'*')))
-		{ &$foo($_[1], $_[0], !$_[2])}
-	else { PDL::mult (@_)}
-}
-
-sub cmi(;@) {
-	my $foo;
-	if (ref $_[1]
-		&& (ref $_[1] ne 'PDL')
-		&& defined ($foo = overload::Method($_[1],'-')))
-		{ &$foo($_[1], $_[0], !$_[2])}
-	else { PDL::minus (@_)}
-}
-
-sub cd(;@) {
-	my $foo;
-	if (ref $_[1]
-		&& (ref $_[1] ne 'PDL')
-		&& defined ($foo = overload::Method($_[1],'/')))
-		{ &$foo($_[1], $_[0], !$_[2])}
-	else { PDL::divide (@_)}
-}
-
-
-  # Used in overriding standard PDL +, -, *, / ops in the complex subclass.
-  use overload (
-		 '+' => \&cp,
-		 '*' => \&cm,
-	         '-' => \&cmi,
-		 '/' => \&cd,
-		);
-
-
-
-        BEGIN{ $^W = $warningFlag;} # Put Back Warnings
-};
+sub re($) { $_[0]->slice("(0)"); }
+sub im($) { $_[0]->slice("(1)"); }
 
 
 {
@@ -177,19 +35,7 @@ sub cd(;@) {
       else{
          return strND($self,$format1,$format2,0);
       }
-   }
 
-
-    sub sum {
-       my($x) = @_;
-       my $tmp = $x->mv(0,1)->clump(0,2)->mv(1,0)->sumover;
-       return $tmp->squeeze;
-    }
-
-   sub sumover{
-      my $m = shift;
-      PDL::Ufunc::sumover($m->xchg(0,1));
-   }
 
 
    sub strND {
@@ -411,5 +257,5 @@ sub cd(;@) {
       $ret .= ' 'x$level."]\n";
       return $ret;
    }
-
+   }
 }
